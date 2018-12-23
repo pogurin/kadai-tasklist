56246f7dabdc79c85268d50b91ff5bef596016e8
mitsuyoshi.toguri <seiun340066@gmail.com>
2018-12-23 14:12:36 +0000

    top page

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :name, presence: true, length: { maximum: 50 }
+  has_secure_password
+end
