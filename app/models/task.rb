bcf93bf0e1cbd53333626784f567fd84ed6ecf7c
mitsuyoshi.toguri <seiun340066@gmail.com>
2018-12-24 04:53:03 +0000

    foriming

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
  validates :status, presence: true, length: { maximum: 10 }
  validates :content, presence: true, length: { maximum: 100 }
+ belongs_to :user
 end
+
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :name, presence: true, length: { maximum: 50 }
   has_secure_password
+  has_many :tasks
 end
